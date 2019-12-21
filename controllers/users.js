6309b8943f21349444e3008dfd28c96cf47aff0d
oldchunkylow <zlow00@e.ntu.edu.sg>
2019-12-21 09:42:53 +0800

    Updated

--- controllers/users.js
+++ controllers/users.js
@@ -6,12 +6,20 @@ module.exports = router;
 
 router.post(`/`, (req, res) => {
   Users.create(req.body, (err, createdUser) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log("WORKING FINE");
     res.json(createdUser);
   });
 });
 
 router.get(`/`, (req, res) => {
   Users.find({}, (err, foundUser) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log("WORKING FINE");
     res.json(foundUser);
   });
 });
@@ -25,8 +33,7 @@ router.delete(`/:id`, (req, res) => {
 router.put(`/:id`, (req, res) => {
   Users.findByIdAndUpdate(
     req.params.id,
-    req.body,
-    {
+    req.body, {
       new: true
     },
     (err, updatedUser) => {
